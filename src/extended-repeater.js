97f73734b4b164d02becc954f1c7f3dbbf3c2d17
Ignat <fun_228@mail.ru>
2023-04-17 18:58:40 +0300

    feat: completed task 8

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+let strToString = str
+let result = ''
+let repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|';
+// проверка на существование свойств
+if (options != false){
+  if (options.repeatTimes != undefined){
+    repeatTimes = options.repeatTimes
+  }
+  if (options.separator != undefined){
+    separator = options.separator
+  }
+  if (options.addition != undefined){
+    addition = options.addition
+  }
+  if (options.additionRepeatTimes != undefined){
+    additionRepeatTimes = options.additionRepeatTimes
+  }
+  if (options.additionSeparator != undefined){
+   additionSeparator = options.additionSeparator
+  }
+}
+if (options.addition === null){
+  addition = null
+}
+let second = addition // addition + additionSeparation * additionRepeatTimes
+if (additionRepeatTimes <= 1){
+  additionSeparator = ''
+} else if (additionRepeatTimes > 1){
+  for (let i = 1; i < additionRepeatTimes; i++){
+    second += additionSeparator + addition // делаю вторую часть строки
+  }
+}
+result = strToString + second // при repeatTimes = 1
+if (repeatTimes > 1){
+  for ( let i = 1; i < repeatTimes; i++){
+    result += separator + strToString + second
+  }
+}
+return result 
 }
 
 module.exports = {
